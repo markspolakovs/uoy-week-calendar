528809f582d4a54fc5f03a6f96f9e867debfa7dd
Christopher Nethercott <ccnethercott@gmail.com>
2023-01-05 20:05:53 +0000

    fix: remove broken testing code in calendar

--- src/calendar.ts
+++ src/calendar.ts
@@ -1,30 +1,11 @@
 import { Request, Response, NextFunction } from "express";
-import ical, { ICalCalendar } from "ical-generator";
-import { getCurrentPeriod, getEndOfPeriod, getFormattedString, getWeeksBetween } from "./period";
+import { ICalCalendar } from "ical-generator";
+import dayjs from "dayjs";
 
 const calendar = new ICalCalendar();
 
 calendar.name("University of York's Week Numbers");
-calendar.description("Providing you with what week we are in."); // todo change this
 
 export async function getCalendar(req: Request, res: Response, next: NextFunction) {
-    const startDate = getCurrentPeriod(new Date()).startDate;
-    const endDate = getEndOfPeriod(new Date());
-
-    console.log(`${startDate} - ${endDate} - ${getWeeksBetween(startDate, endDate)}`);
-
-    for(let i = 0; i < getWeeksBetween(startDate, endDate); i++) {
-        let currentDate = new Date(startDate.setDate(startDate.getDate() + i * 7));
-
-        console.log(`${getFormattedString(currentDate)}`);
-
-        calendar.createEvent({
-            start: currentDate,
-            end: currentDate,
-            allDay: true,
-            summary: getFormattedString(currentDate)
-        });
-    }
-
     return calendar.serve(res);
 }
