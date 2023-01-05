518358d0588956e12f86d00a217331b19a640575
Christopher Nethercott <ccnethercott@gmail.com>
2023-01-05 20:11:42 +0000

    feat: create example event that is all day and only occurs on today

--- src/calendar.ts
+++ src/calendar.ts
@@ -1,10 +1,15 @@
 import { Request, Response, NextFunction } from "express";
 import { ICalCalendar } from "ical-generator";
-import dayjs from "dayjs";
 
 const calendar = new ICalCalendar();
 
 calendar.name("University of York's Week Numbers");
+calendar.createEvent({
+    start: new Date(),
+    end: new Date(),
+    allDay: true,
+    summary: 'My first event'
+})
 
 export async function getCalendar(req: Request, res: Response, next: NextFunction) {
     return calendar.serve(res);
