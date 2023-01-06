37f7e5b5a81b8a36098ab94e209efa09ed35a8dc
Christopher Nethercott <ccnethercott@gmail.com>
2023-01-06 11:22:00 +0000

    test: remove Calendar.test.ts to be replaced with the calendar test later

--- test/calendar.test.ts
+++
@@ -1,7 +0,0 @@
-import {academicYears} from "../src/period";
-
-describe('Calendar Testing Suite', () => {
-   it('AcademicYear should be an array', () => {
-       expect(academicYears).toBeDefined();
-   })
-});
