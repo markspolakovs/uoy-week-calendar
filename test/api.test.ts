8b1b95c4f2bb1302f87dde93ca28344748d09db0
Christopher Nethercott <ccnethercott@gmail.com>
2023-01-14 10:47:09 +0000

    test: api route is /api/ping not /api/pong

--- test/api.test.ts
+++ test/api.test.ts
@@ -2,7 +2,7 @@ import supertest from "supertest";
 import {app} from "../src/app";
 
 describe('API Methods', () => {
-    it('GET /api/pong should be 200', async () => {
+    it('GET /api/ping should be 200', async () => {
         const response = await supertest(app).get('/api/ping')
 
         expect(response.statusCode).toBe(200);
