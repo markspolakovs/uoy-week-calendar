9108ff850af1d5b030186b500828316ff8df29d5
Christopher Nethercott <ccnethercott@gmail.com>
2023-01-18 17:14:45 +0000

    chore: remove app / api version of app

--- src/routes/api/pong.ts
+++
@@ -1,8 +0,0 @@
-import {Request, Response, NextFunction} from "express";
-import dayjs from "dayjs";
-
-export default function getPong(req: Request, res: Response, next: NextFunction) {
-    res.json({
-        "pong": dayjs()
-    })
-}
--- src/server.ts
+++
@@ -1,5 +0,0 @@
-import {app} from "./app";
-
-const port = process.env.PORT ?? 9000;
-
-app.listen(port);
